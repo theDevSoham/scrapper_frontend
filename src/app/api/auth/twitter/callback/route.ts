ff120aebdf6b5a4b85d900615f556ee672c4c13d
Soham Das <publicsoham.24@gmail.com>
2025-11-23 17:17:42 +0530

    fix: twitter redirect uri mismatch fix

--- src/app/api/auth/twitter/callback/route.ts
+++ src/app/api/auth/twitter/callback/route.ts
@@ -17,7 +17,7 @@ export async function GET(req: NextRequest) {
   const body = new URLSearchParams({
     grant_type: "authorization_code",
     client_id: process.env.TWITTER_CLIENT_ID!,
-    redirect_uri: "http://localhost:3000/api/auth/twitter/callback",
+    redirect_uri: process.env.TWITTER_REDIRECT_URI!,
     code,
     code_verifier,
   });
